03a121331abeadfda9b51e183cdd0b3373383695
Sampath Kumar Bingi <sampath42@gmail.com>
2018-07-31 19:31:34 +0530

    Dot Net Mentoring program batch3 preparations
    Dot Net Mentoring program batch3 preparations

---
+++ M7/DelegatesAndEvents/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace DelegatesAndEvents
+{
+    public delegate void WorkPerformedHandler(int hours, string workToDo);
+    public delegate int WorkPerformedHandlerReturn(int hours, string workToDo);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // WorkPerformedHandler user1 = new WorkPerformedHandler(WorkPerformed_Work1);
+            // WorkPerformedHandler user2 = new WorkPerformedHandler(WorkPerformed_Work2);
+
+            // user1(10,"work 1");
+            // user2(20,"work 2");
+
+            // DoWork(10,WorkPerformed_Work1);
+
+            //===============================================//
+
+            // WorkPerformedHandlerReturn user1_1 = new WorkPerformedHandlerReturn(WorkPerformed_Work1_1);
+            // WorkPerformedHandlerReturn user2_1 = new WorkPerformedHandlerReturn(WorkPerformed_Work2_1);            
+
+            // var hoursSpentForWork1 = user1_1(10,"work 1");
+            // var hoursSpentForWork2 = user2_1(10,"work 2");            
+
+            // Console.WriteLine(hoursSpentForWork1);
+            // Console.WriteLine(hoursSpentForWork2);
+
+            //===============================================//
+
+            // WorkPerformedHandlerReturn multicastUser1 = new WorkPerformedHandlerReturn(WorkPerformed_Work1_1);
+            // WorkPerformedHandlerReturn multicastUser2 = new WorkPerformedHandlerReturn(WorkPerformed_Work2_1);            
+
+            // multicastUser1 += new WorkPerformedHandlerReturn(WorkPerformed_Work3_1);
+
+            // var hoursSpentForWork1 = multicastUser1(10,"work 1");
+            // var hoursSpentForWork2 = multicastUser2(10,"work 2");            
+
+            // Console.WriteLine(hoursSpentForWork1);
+            // Console.WriteLine(hoursSpentForWork2);
+            
+        }
+
+        static void DoWork(int hours, WorkPerformedHandler workPerformed_Work1)
+        {
+            workPerformed_Work1(hours,"work to do");
+        }
+
+        static void WorkPerformed_Work1(int hours, string workToDo)
+        {
+            // Work 1 related implementation;
+            Console.WriteLine($"{workToDo} in {hours} Hrs");
+        }
+
+        static void WorkPerformed_Work2(int hours, string workToDo)
+        {
+            //Work 2 related implementation;
+            Console.WriteLine($"{workToDo} in {hours} Hrs");
+        }
+        static int WorkPerformed_Work1_1(int hours, string workToDo)
+        {
+            // Work 1 related implementation;
+            Console.WriteLine($"{workToDo} in {hours} Hrs");
+            return hours + 5;
+        }
+
+        static int WorkPerformed_Work2_1(int hours, string workToDo)
+        {
+            //Work 2 related implementation;
+            Console.WriteLine($"{workToDo} in {hours} Hrs");
+            return hours + 2;
+        }
+
+        static int WorkPerformed_Work3_1(int hours, string workToDo)
+        {
+            //Work 2 related implementation;
+            Console.WriteLine($"{workToDo} in {hours} Hrs");
+            return hours + 8;
+        }
+    }
+}
